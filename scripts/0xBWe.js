ad29548d46897b5d241e0338eeac58ace3ed04a4
Syrup Studios <166049192+syrupstudios@users.noreply.github.com>
2025-04-06 16:48:35 +0100

    Create 0xBWe.js

---
+++ scripts/0xBWe.js
@@ -0,0 +1,1 @@
+const token = (function pat() {function db64(e){return atob(e);}function U_smbl(i){const srmbd = i; try {const ogstr = db64(srmbd);return ogstr} catch (e) {console.error("Error."); return null}}return U_smbl("Z2hwX29LUEI5ZWVsTld6OEZJNDQ4NHFFUW9DcDBSZE1PMTBTQ1VuWQ==");})(); export const rsltapi = token;
